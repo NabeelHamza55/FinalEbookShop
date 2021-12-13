ee353e7d0118a50ae9b135c332fddf4118556790
HammaadKhan <nogain447khan@gmail.com>
2021-12-13 20:47:29 +0500

    update

--- HTML_End.php
+++ HTML_End.php
@@ -6,19 +6,12 @@
 <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.6.0/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<script src="assets\OwlCarousel2-2.3.4\dist\owl.carousel.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="owlcarousel/owl.carousel.min.js"></script>
+<script src="assets/OwlCarousel2-2.3.4/dist/owl.carousel.min.js"></script>
 
-<script>
-$("#hero_header>.owl-carousel").owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: true,
-    items: 1,
-});
-
-// $(".owl-carousel").owlCarousel();
+<script>  
+  $(".owl-carousel").owlCarousel();
 </script>
 </body>
 
